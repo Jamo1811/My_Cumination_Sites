4e7c97bf03ff5b03080696f416d258d8ef7bb094
Jamo1811 <DirtyJamila1811@gmx.de>
2026-08-20 02:35:47 +0200

    Add Item class with initial properties

---
+++ plugin.video.mycuminationsites/resources/lib/models/item.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+class Item:
+    def __init__(self):
+        self.title = ""
+        self.url = ""
+        self.image = ""
+        self.action = "play_video"
+        self.is_folder = False
